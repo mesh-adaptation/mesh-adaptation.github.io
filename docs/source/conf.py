90431af8db53f87b3aa715b0a40c732712f9e77c
acse-ej321 <acse-ej321@github.com>
2024-01-25 16:34:18 +0000

    add custom css into theme options

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -111,7 +111,7 @@ html_theme_path = ["_themes"]
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = []
+html_static_path = ['_static/custom.css']
 
 # Add custom static files (such as style sheets) to override all style themes. 
 # This is sphinx specific and not a theme-specific option. 
